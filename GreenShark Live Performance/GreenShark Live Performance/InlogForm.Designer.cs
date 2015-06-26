11282f3997a982c321da7a3eaee84b23d6fca454
AwesomeKevin <k.kopp@student.fontys.nl>
2015-06-26 11:01:58 +0200

    Opstart
    De applicatie is aangemaakt, het LogInForm is ook al gemaakt.

---
+++ GreenShark Live Performance/GreenShark Live Performance/InlogForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace GreenShark_Live_Performance
+{
+    partial class InlogForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWelkom = new System.Windows.Forms.Label();
+            this.tbGebruikersnaam = new System.Windows.Forms.TextBox();
+            this.lblGebruikersnaam = new System.Windows.Forms.Label();
+            this.lblWachtwoord = new System.Windows.Forms.Label();
+            this.tbWachtwoord = new System.Windows.Forms.TextBox();
+            this.btnInloggen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblWelkom
+            // 
+            this.lblWelkom.AutoSize = true;
+            this.lblWelkom.Location = new System.Drawing.Point(45, 9);
+            this.lblWelkom.Name = "lblWelkom";
+            this.lblWelkom.Size = new System.Drawing.Size(182, 13);
+            this.lblWelkom.TabIndex = 0;
+            this.lblWelkom.Text = "Welkom bij de GreenShark applicatie\r\n";
+            // 
+            // tbGebruikersnaam
+            // 
+            this.tbGebruikersnaam.Location = new System.Drawing.Point(141, 54);
+            this.tbGebruikersnaam.Name = "tbGebruikersnaam";
+            this.tbGebruikersnaam.Size = new System.Drawing.Size(100, 20);
+            this.tbGebruikersnaam.TabIndex = 1;
+            // 
+            // lblGebruikersnaam
+            // 
+            this.lblGebruikersnaam.AutoSize = true;
+            this.lblGebruikersnaam.Location = new System.Drawing.Point(45, 57);
+            this.lblGebruikersnaam.Name = "lblGebruikersnaam";
+            this.lblGebruikersnaam.Size = new System.Drawing.Size(90, 13);
+            this.lblGebruikersnaam.TabIndex = 2;
+            this.lblGebruikersnaam.Text = "Gebruikersnaam: ";
+            // 
+            // lblWachtwoord
+            // 
+            this.lblWachtwoord.AutoSize = true;
+            this.lblWachtwoord.Location = new System.Drawing.Point(48, 95);
+            this.lblWachtwoord.Name = "lblWachtwoord";
+            this.lblWachtwoord.Size = new System.Drawing.Size(74, 13);
+            this.lblWachtwoord.TabIndex = 3;
+            this.lblWachtwoord.Text = "Wachtwoord: ";
+            // 
+            // tbWachtwoord
+            // 
+            this.tbWachtwoord.Location = new System.Drawing.Point(141, 92);
+            this.tbWachtwoord.Name = "tbWachtwoord";
+            this.tbWachtwoord.Size = new System.Drawing.Size(100, 20);
+            this.tbWachtwoord.TabIndex = 4;
+            // 
+            // btnInloggen
+            // 
+            this.btnInloggen.Location = new System.Drawing.Point(96, 153);
+            this.btnInloggen.Name = "btnInloggen";
+            this.btnInloggen.Size = new System.Drawing.Size(75, 23);
+            this.btnInloggen.TabIndex = 5;
+            this.btnInloggen.Text = "Log In";
+            this.btnInloggen.UseVisualStyleBackColor = true;
+            // 
+            // InlogForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 198);
+            this.Controls.Add(this.btnInloggen);
+            this.Controls.Add(this.tbWachtwoord);
+            this.Controls.Add(this.lblWachtwoord);
+            this.Controls.Add(this.lblGebruikersnaam);
+            this.Controls.Add(this.tbGebruikersnaam);
+            this.Controls.Add(this.lblWelkom);
+            this.Name = "InlogForm";
+            this.Text = "Inloggen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWelkom;
+        private System.Windows.Forms.TextBox tbGebruikersnaam;
+        private System.Windows.Forms.Label lblGebruikersnaam;
+        private System.Windows.Forms.Label lblWachtwoord;
+        private System.Windows.Forms.TextBox tbWachtwoord;
+        private System.Windows.Forms.Button btnInloggen;
+    }
+}
+

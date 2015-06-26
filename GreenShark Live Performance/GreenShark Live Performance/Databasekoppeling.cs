3ad7c732b9bd4745b69770361659e72adc99ff1f
AwesomeKevin <k.kopp@student.fontys.nl>
2015-06-26 14:56:14 +0200

    Database
    Databaseklasse aangemaakt, maar nog niet getest ivm pauze.

---
+++ GreenShark Live Performance/GreenShark Live Performance/Databasekoppeling.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+using System.Data;
+
+namespace GreenShark_Live_Performance
+{
+    public class Databasekoppeling
+    {
+        //Fields
+        private OracleConnection conn;
+        private OracleCommand command;
+        string user = "dbi318713"; //Dit is de gebruikersnaam
+        string pw = "V7brKp3nww"; //Dit is het wachtwoord
+
+        public Databasekoppeling()
+        {
+            conn = new OracleConnection();
+            command = conn.CreateCommand();
+            conn.ConnectionString = "User Id=" + user + ";Password=" + pw + ";Data Source=" + "//192.168.15.50:1521/fhictora" + ";";
+        }
+    }
+}
